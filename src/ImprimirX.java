d9abb4cf2efe4438d3b5ab6c0dbc0de0b8d3683f
RafaIlerna <113125370+RafaIlerna@users.noreply.github.com>
2022-11-25 17:24:24 +0100

    Update ImprimirX.java
    Prueba:
    he añadido la respuesta esperada y no la mia

--- src/ImprimirX.java
+++ src/ImprimirX.java
@@ -9,6 +9,12 @@ public class ImprimirX {
 			for( int j = n; j > i; j-- )
 			System.out.print(n);
 			System.out.println();
+//			Respuesta
+/*		for( int i = 1; i <= n; i++ ) {
+			for( int j = 1; j <= i; j++ )
+				System.out.print(i);
+			System.out.println();
+		}*/
 		}	
 	}
 }

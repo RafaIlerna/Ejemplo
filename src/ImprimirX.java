d96ebf3edfe711fd579cd2a2244aace4a601cb01
RafaIlerna <113125370+RafaIlerna@users.noreply.github.com>
2022-11-25 17:30:24 +0100

    Prueba de la primera rama
    Ahora deberia imprimir los numeros en orden invertido

--- src/ImprimirX.java
+++ src/ImprimirX.java
@@ -5,16 +5,17 @@ public class ImprimirX {
 		System.out.println("Dame un numero y are una cosita");
 		Scanner s = new Scanner(System.in);
 		int n = s.nextInt();
-		for( int i = 0; i < n; n-- ) {
+/*		for( int i = 0; i < n; n-- ) {
 			for( int j = n; j > i; j-- )
 			System.out.print(n);
 			System.out.println();
+*/
 //			Respuesta
-/*		for( int i = 1; i <= n; i++ ) {
+		for( int i = 1; i <= n; i++ ) {
 			for( int j = 1; j <= i; j++ )
 				System.out.print(i);
 			System.out.println();
-		}*/
+		
 		}	
 	}
 }

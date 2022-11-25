072bcf4d99778bc756295c645b7547768bc1e525
RafaIlerna <113125370+RafaIlerna@users.noreply.github.com>
2022-11-25 17:44:17 +0100

    ...
    ¿Que coño esta pasando?

--- src/ImprimirX.java
+++ src/ImprimirX.java
@@ -5,6 +5,7 @@ public class ImprimirX {
 		System.out.println("Dame un numero y are una cosita");
 		Scanner s = new Scanner(System.in);
 		int n = s.nextInt();
+<<<<<<< Updated upstream
 		for( int i = 0; i < n; n-- ) {
 			for( int j = n; j > i; j-- )
 			System.out.print(n);
@@ -13,6 +14,13 @@ public class ImprimirX {
 /*		for( int i = 1; i <= n; i++ ) {
 			for( int j = 1; j <= i; j++ )
 				System.out.print(i);
+=======
+		for( int i = 1; i <= n; i++ ) {
+			for( int j = 1; j <= n - i; j++ )
+				System.out.print(" "); 
+			for( int j = 1; j <= 2 * i - 1; j++) //Esto creara una piramide
+				System.out.print("*");
+>>>>>>> Stashed changes
 			System.out.println();
 		}*/
 		}	
